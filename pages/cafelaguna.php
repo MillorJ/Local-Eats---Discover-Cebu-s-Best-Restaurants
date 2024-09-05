18d1de2a7bce071cb3e97207edb07b83aa7dae51
John Rey Millor <johnreymillor69@gmail.com>
2024-09-05 21:00:05 +0800

    Update cafelaguna.php

--- pages/cafelaguna.php
+++ pages/cafelaguna.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+// Redirect to login page if the user is not logged in
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header('Location: login.php?show_overlay=true');
     exit;
